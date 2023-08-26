7fae65c6f43be1c3572fdc535980b1e65dc124d5
Murilo Goivino Tegani <murilo.tegani@ibm.com>
2023-08-26 12:47:01 -0300

    initial commit

---
+++ src/test/java/br/gov/sp/cptm/BikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
